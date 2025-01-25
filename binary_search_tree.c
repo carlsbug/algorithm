af161d668cdfb34abfcc2985390dc76c346d4842
kwmin605@gmail.com <kwmin605@gmail.com>
2025-01-25 18:02:36 +0900

    Add search

--- binary_search_tree.c
+++ binary_search_tree.c
@@ -7,13 +7,13 @@ struct Node{
     struct Node *right;
 };
 
-void traverse(struct Node* n){
+void traverse_inorder(struct Node* n){
     if (n == NULL){
         return;
     }
-    traverse(n->left);
+    traverse_inorder(n->left);
     printf("%d ", n->key);
-    traverse(n->right);
+    traverse_inorder(n->right);
 } 
 
 struct Node* insertNode(struct Node* n, int key){
@@ -30,11 +30,27 @@ struct Node* insertNode(struct Node* n, int key){
     }
 }
 
+struct Node* search(struct Node* n, int key){
+    if (n == NULL){
+        return NULL;
+    }
+    else if (n->key == key){
+        printf("found node, key: %d", n->key);
+        return NULL;
+    }
+    else if (n->key > key){
+        return search(n->left, key);
+    }
+    else{
+        return search(n->right, key);
+    }
+}
+
 int main(){
     struct Node tree = {5, NULL, NULL};
     struct Node *ptr = &tree;
 
     insertNode(ptr, 10);
-    traverse(ptr);
-
+    traverse_inorder(ptr);
+    struct Node *found = search(ptr, 10);
 }
